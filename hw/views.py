a933e82d3c16e9685b0f82c05855cb4ab7c89e56
SaeginChang <nechang@bu.edu>
2024-09-17 17:46:19 -0400

    class examples

---
+++ hw/views.py
@@ -0,0 +1,37 @@
+## hw/views.py
+## description: write view functions to handle URL requests for the hw app
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+import time
+import random
+
+# # Create your views here.
+# def home(request):
+#     '''Handle the main URL for the hw app.'''
+
+#     response_text = '''
+#     Hello, world!
+#     '''
+
+#     # create and return a reponse to the client
+#     return HttpResponse(response_text)
+
+def home(request):
+    '''
+    Function to handle the URL request for /hw (main page).
+    Delegate redering to the temmplate hw/home.html
+    '''
+
+    # use this template to reder the response
+    template_name = 'hw/home.html'
+
+    # create a dictionary of context variables for the template:
+    context = {
+        "current_time" : time.ctime(),
+        "letter1" : chr(random.randit(65,90)), # aletter from A to Z
+        "letter2" : chr(random.randit(65,90)),
+        "number" : random.randint(1,10), # number from 1 to 10
+    }
+
+    # delegate redering work to the template
+    return render(request, template_name, context)
