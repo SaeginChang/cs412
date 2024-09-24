6abc529d197e461ecd5271cd45db706e596af387
SaeginChang <nechang@bu.edu>
2024-09-24 11:43:22 -0400

    urls.py for rest

--- formdata/views.py
+++ formdata/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def show_form(request):
+    '''
+    Show the contact form.
+    '''
+    
+    template_name = "formadata/form.html"
+    return render(request, template_name)
